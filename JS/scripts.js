e0bb80645e5b0f4f83cb627d093395217b03507e
Ilya Vlasov <ilyavlasov93@gmail.com>
2020-09-14 10:39:55 -0400

    1.1->1.2 HW
    Script will be changed fully for new Home Work.
    1.1 -> 1.2
    It used to be varible practis, now it will become pokmon list for new project

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,11 @@
-let myName = "Ilya";
 
-var favoriteFood = "Steak";
+let pokemonList1=[
+	{name: "Bulbasar", hight: 2.04, type: ["Grass", "poison"]},
+	{name: "Charmander", hight: 2, type: "Fire"}
+];
+let pokemonList2 = [
+	{name: "Squirtle", hight: 1.08, type: "water"},
+	{name: "Pikachu", hight: 1.04, type: "Electric"}
+];
 
-document.write(myName);
-
-myName = "John";
-
-document.write(myName);
-
-alert('Hello World');
-
-document.write(favoriteFood);
+let pokemons = pokemonList1 + pokemonList2;
