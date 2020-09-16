24651e77ddc3dd3ad9d1d2ce4cace4ae8738631d
Ilya Vlasov <Fox20073@users.noreply.github.com>
2020-09-16 10:35:19 -0400

    1.3
    I made all changes by assigment, but im not sure how to make CSS rules for script. Please mark it as aditinal comment to assigment.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,60 @@
-
+//2 lists of pokemons for project/ 
 let pokemonList1=[
 	{name: "Bulbasar", hight: 2.04, type: ["Grass", "poison"]},
 	{name: "Charmander", hight: 2, type: "Fire"}
 ];
+
 let pokemonList2 = [
 	{name: "Squirtle", hight: 1.08, type: "water"},
 	{name: "Pikachu", hight: 1.04, type: "Electric"}
 ];
 
 let pokemons = pokemonList1 + pokemonList2;
+
+/For loop for both pokemo lists/ 
+for (let i = 0; i < pokemonList1.length; i++) {
+	document.write(" Pokemons' " + pokemonList1[i].name + " hight is " + pokemonList1[i].hight + ".</br>")
+};
+
+for (let i = 0; i < pokemonList2.length; i++) {
+	document.write("pokemons' hight is " + pokemonList2[i].hight + ".</br>")
+}; 
+
+//Organizing the code for assigments/ 
+
+let repository = [
+	{
+		name: "Bulbasar", 
+		hight: 2.04, 
+		type: ["Grass", "poison"]	
+	},
+	{
+		name: "Charmander", 
+		hight: 2, 
+		type: "Fire"
+	},
+	{
+		name: "Squirtle", 
+		hight: 1.08, 
+		type: "water"
+	},
+	{name: "Pikachu", 
+	hight: 1.04, 
+	type: "Electric"
+    }
+];
+ 
+// Using if statment to specify pokemons by size /
+
+for (let i=0; i < repository.length; i++){
+	if (repository[i].hight <= 1.5){
+		console.log (repository[i].name + " " + repository[i].type + " is small pokemon! ");
+		document.write (repository[i].name + " is small pokemon! </br>" );
+	} else if (repository[i].hight > 1.5 && repository[i].hight <= 2){
+		console.log (repository[i].name + " " + repository[i].type + " is avarege pokemon! ")
+		document.write (repository[i].name + " is avarege pokemon! </br>");
+	} else {
+		console.log (repository[i].name + " " + repository[i].type + " is HUGE pokemon!!! ")
+		document.write (repository[i].name + " is BIG pokemon!!!! </br>");
+	}
+}
