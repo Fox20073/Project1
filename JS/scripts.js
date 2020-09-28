c775ae3ed25dda3c9cc7795be947c591520f8562
Ilya Vlasov <Fox20073@users.noreply.github.com>
2020-09-28 11:53:10 -0400

    1.5 -> 1.6
    Please see new code

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,74 +1,88 @@
 //Organizing the code for assignment IIFT/ 
 var pokemonRepository = (function() {
-	var pokemonList= [
-		{
-			name: "Bulbasar", 
-			hight: 2.04, 
-			type: ["Grass", " Poison"]	
-		},
-		{
-			name: "Charmander", 
-			hight: 2, 
-			type: "Fire"
-		},
-		{
-			name: "Squirtle", 
-			hight: 1.08, 
-			type: "water"
-		},
-		{
-			name: "Pikachu", 
-			hight: 1.04,
-			type: "Electric"
-		}
-	];
+	var pokemonList= [];
+	var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-	function getAll() {
-		return pokemonList;
-	};
 
 	function add(pokemon) {
 		pokemonList.push(pokemon)
 	};
+	
+	function getAll() {
+		return pokemonList;
+	};
+
+	
 	function addListItem (pokemon){
-		let pokedexList = document.querySelector(".pokemon-list");
-		let listPokedex = document.createElement('li');
+		let pokemonList = document.querySelector(".pokemon-list");
+		let listPokemon = document.createElement('li');
 		let button = document.createElement("button");
 
 		button.innerText = pokemon.name;
 		button.classList.add("button");
-		listPokedex.appendChild(button);
-		pokedexList.appendChild(listPokedex);
+		listPokemon.appendChild(button);
+		pokemonList.appendChild(listPokemon);
 
-		button.addEventListener("click", function(event){showDetails(pokemon.name);});
+		button.addEventListener("click", function(event){showDetails(pokemon);
+		});
 	};
 
-	function showDetails(pokemon){
-		console.log(pokemon)
-	} 
 
+	function loadList() {
+		return fetch(apiUrl).then(function (response) {
+		  return response.json();
+		}).then(function (json) {
+		  json.results.forEach(function (item) {
+			let pokemon = {
+			  name: item.name,
+			  detailsUrl: item.url
+			};
+			add(pokemon);
+			console.log(pokemon);
+		  });
+		}).catch(function (e) {
+		  console.error(e);
+		})
+	  };
 
+	  function loadDetails(item) {
+		let url = item.detailsUrl;
+		return fetch(url).then(function (response) {
+		  return response.json();
+		}).then(function (details) {
+		  // Now we add the details to the item
+		  item.imageUrl = details.sprites.front_default;
+		  item.height = details.height;
+		  item.types = details.types;
+		}).catch(function (e) {
+		  console.error(e);
+		});
+	  };
+
+	  function showDetails(pokemon) {
+		loadDetails(pokemon).then(function () {
+		  console.log(pokemon);
+		});
+	  };
+
+	  
 	return {
 		getAll: getAll,
 		add: add,
 		addListItem: addListItem,
-		showDetails: showDetails
+		loadList: loadList,
+		loadDetails: loadDetails,
+		showDetails: showDetails,
 	};
 })();
 
 // Practising with IIFT call
 
 
-
-
-pokemonRepository.add({name: "Psy Duck", hight: 2, type:"Electricity"});
-
-
-
-pokemonRepository.add({name: "Pidgey", hight: 1, type:["Flying", "Air"]});
-
-pokemonRepository.getAll().forEach(function(pokemon){
-pokemonRepository.addListItem(pokemon)
-});
-
+pokemonRepository.loadList().then(function() {
+	// Now the data is loaded!
+	pokemonRepository.getAll().forEach(function(pokemon){
+	  pokemonRepository.addListItem(pokemon);
+	});
+  });
 
