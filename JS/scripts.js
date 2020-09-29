5c6f57345209cfd4d94557e45f777dfc1c572707
Ilya Vlasov <Fox20073@users.noreply.github.com>
2020-09-29 13:23:29 -0400

    1.8
    Hi there .
    Please see added module java and other updates, such as HTML uptimisation, CSS changes and cross-browsing optimisation.

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,7 +2,7 @@
 var pokemonRepository = (function() {
 	var pokemonList= [];
 	var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-
+	let modalContainer = document.querySelector ('#pokemon-modal');
 
 	function add(pokemon) {
 		pokemonList.push(pokemon)
@@ -61,11 +61,65 @@ var pokemonRepository = (function() {
 
 	  function showDetails(pokemon) {
 		loadDetails(pokemon).then(function () {
-		  console.log(pokemon);
+		  showModal(pokemon);
 		});
 	  };
 
-	  
+	  function showModal(pokemon) {
+		modalContainer.innerHTML = "";
+		let modal = document.createElement('div');
+		modal.classList.add("modal");
+
+		let closeButtonElement = document.createElement('button');
+		closeButtonElement.classList.add("modal-close");
+		closeButtonElement.innerHTML = "Back to Pokedex";
+		closeButtonElement.addEventListener('click', hideModal);
+
+
+		let titleElement = document.createElement('h1');
+		titleElement.innerHTML = pokemon.name;
+
+		let containerElement = document.createElement('p');
+		containerElement.innerHTML = "Hight is " + pokemon.height;
+
+
+		let myImage = document.createElement('img');
+		myImage.src = pokemon.imageUrl
+		
+		
+		
+		modal.appendChild(closeButtonElement);
+		modal.appendChild(titleElement);
+		modal.appendChild(closeButtonElement);
+		modal.appendChild(containerElement);
+		modal.appendChild(myImage);
+		modalContainer.appendChild(modal);
+
+
+		modalContainer.classList.add("visible");
+	};
+
+	window.addEventListener('keydown', (e) => {
+		if (e.key === 'Escape' && modalContainer.classList.contains('visible')) {
+			hideModal();
+		}
+	});
+
+	
+	modalContainer.addEventListener('click', (e) => {
+		let target = e.target;
+		if (target === modalContainer) {
+			hideModal();
+		}
+	});
+
+
+
+	function hideModal() {
+		modalContainer.classList.remove('visible'); 
+	};
+
+
 	return {
 		getAll: getAll,
 		add: add,
@@ -73,6 +127,8 @@ var pokemonRepository = (function() {
 		loadList: loadList,
 		loadDetails: loadDetails,
 		showDetails: showDetails,
+		showModal: showModal,
+		hideModal: hideModal,
 	};
 })();
 
@@ -85,4 +141,8 @@ pokemonRepository.loadList().then(function() {
 	  pokemonRepository.addListItem(pokemon);
 	});
   });
+
+  document.querySelector('.button').addEventListener('click', () => {
+	  showModal(pokemon);
+  });	
 
